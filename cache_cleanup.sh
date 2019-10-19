52a947b8cbdc9e66c189d31b957aeaa0a4ab32ee
Shohei Nakagawa <hukusuke1007@gmail.com>
2019-10-19 21:22:51 +0900

    Update cache_cleanup.sh

--- cache_cleanup.sh
+++ cache_cleanup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -x
 # Thank you. https://qiita.com/kiyodori/items/0fcc87bb8ab1cb31c731
-# sudo du -x -m -d 5 / | awk '$1 >= 1000{print}'
 
 rm -rf ~/Library/Application\ Support/Adobe/Common/Media\ Cache/* ~/Library/Application\ Support/Adobe/Common/Media\ Cache\ Files/*
 sudo rm -r /private/var/vm/sleepimage
